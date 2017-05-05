8d91552e379279a9981a40b5d65275fd3641bc65
DK\dmsa <dmsa@sitecore.net>
2017-05-05 11:17:51 +0300

    Commit

---
+++ src/Sitecore.Support.67803/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.67803")]
+[assembly: AssemblyProduct("Sitecore.Support.67803")]
+[assembly: ComVisible(false)]
